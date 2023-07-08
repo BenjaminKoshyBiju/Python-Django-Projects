212ddd875ca98d6face0b1538a0db628a36d99f8
BenjaminKoshyBiju <bennn455@gmail.com>
2023-07-08 10:08:48 +0530

    further learning

--- calc/views.py
+++ calc/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request,'index.html',{'name':"Benjamin Koshy Biju"})
+    return render(request,'index.html',{'name':"Benjamin Koshy Biju",'quote':'I will succeed'})
 # def calculator(request):
 #     return HttpResponse(5+6)
 
