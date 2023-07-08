17ca45585e9b4803daaccdd2a33f6caffbf73f58
BenjaminKoshyBiju <bennn455@gmail.com>
2023-07-08 23:06:52 +0530

    calc learning completed
    completed working on this small project gonna start a new app now with a template from net for UI

--- calc/urls.py
+++ calc/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path("",views.index,name="index"),
-    #path("b",views.calculator,name="Retn_calculation")
+   path("result",views.result,name="result")
+   
 ]
--- calc/views.py
+++ calc/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html',{'name':"Benjamin Koshy Biju",'quote':'I will succeed'})
-# def calculator(request):
-#     return HttpResponse(5+6)
+def result(request):
+    num1=int(request.POST['n1'])
+    num2=int(request.POST['n2'])
+    n3=num1+num2
+    return render(request,'result.html',{'res':n3})
 
