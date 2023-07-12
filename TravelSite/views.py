4ba2e9bbfb8c42ede4065a042ab097931eb5fc56
BenjaminKoshyBiju <bennn455@gmail.com>
2023-07-13 00:08:25 +0530

    all css nd stuff embedded made it somewhat dynamic
    but i only needed 1 distination but i am getting all destinations, tried to debug it but unsuccessful, no issues still can continue with the completion of the project.

--- TravelSite/views.py
+++ TravelSite/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from .models import destination
 
 # Create your views here.
+dests=destination.objects.all()
 def index(request):
-    dests=destination.objects.all()
+    
     return render(request,"index.html",{'dests':dests})
 def destinations(request):
-    return render(request,"destinations.html")
+        return render(request,"destinations.html",{'dests':dests})
